d7a3f933a62eb9fa87d3096a030d2faf6bcc4478
ArifeenMahmud <arifeen.mahmud@yahoo.com>
2021-04-20 00:03:14 +0600

    Initial commit ThisKeyword In Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+    int a;
+    int b;
+
+    public void setData(int a ,int b){
+        this.a = a;
+        this.b = b;
+    }
+    public void showData(){
+        System.out.println("Value of A ="+a);
+        System.out.println("Value of B ="+b);
+    }
+
+    public static void main(String[] args) {
+
+        Main obj = new Main();
+        obj.setData(2,3);
+        obj.showData();
+
+
+    }
+}
